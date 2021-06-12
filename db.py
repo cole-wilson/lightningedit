1258f087789127499c20668281a52f6dc81dac2a
Cole Wilson <49500253+cole-wilson@users.noreply.github.com>
2021-06-12 04:17:16 +0000

    a

--- db.py
+++ db.py
@@ -25,13 +25,13 @@ class SQL:
 			self.db = mysql.connector.connect(database='main', **setup)
 
 		self.cursor = self.db.cursor(buffered=True)
-		self.cursor.execute("SHOW TABLES")
+		# self.cursor.execute("SHOW TABLES")
 		# if 'users' not in self.cursor:
 		# 	self.cursor.execute(
 		# 		"CREATE TABLE users (id VARCHAR(15), token VARCHAR(400))"
 		# 	)
 		self.cursor.execute('SELECT id FROM users')
-		self.users = self.cursor.fetchall()
+		self.users = list(map(lambda i: [0], self.cursor.fetchall()))
 
 	def __setitem__(self, key, value):
 		if key in self.users:
@@ -40,10 +40,10 @@ class SQL:
 			self.cursor.execute("INSERT INTO users (id, token) VALUES (%s, %s)", (key, value))
 		self.db.commit()
 		self.cursor.execute('SELECT id FROM users')
-		self.users = self.cursor.fetchall()
+		self.users = list(map(lambda i: [0], self.cursor.fetchall()))
 
 	def __getitem__(self, key):
-		return self.cursor.execute('SELECT token WHERE id=%s', [key]).fetchone()
-
+		return self.cursor.execute('SELECT token WHERE id=%s', [key]).fetchone()[0]
+		
 	def keys(self):
 		return self.users
