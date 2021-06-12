40cb86561cf03e658e66a17eddef2a81865c4e4d
Cole Wilson <49500253+cole-wilson@users.noreply.github.com>
2021-06-12 03:20:50 +0000

    fix db?

--- db.py
+++ db.py
@@ -1,15 +1,15 @@
 import os
 import mysql.connector
 
-for i in os.environ:
-	print(i, os.environ[i])
+# for i in os.environ:
+# 	print(i, os.environ[i])
 
-# mydb = mysql.connector.connect(
-#   host=os.environ['MYSQLHOST'],
-#   user=os.environ['MYSQLUSER'],
-#   password=os.environ['MYSQLPASSWORD'],
-# 	# port=os.environ['MYSQLPORT']
-# )
+mydb = mysql.connector.connect(
+  host=os.environ['MYSQLHOST'],
+  user=os.environ['MYSQLUSER'],
+  password=os.environ['MYSQLPASSWORD'],
+	port=os.environ['MYSQLPORT']
+)
 
 class SQL:
 	def __setitem__(self, key, value):
